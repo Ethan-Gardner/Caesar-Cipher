f91e8770ec9ea0ae99ac9b5c2e159a4faff022a3
Ethan Gardner <gardnere@mdsd.org>
2024-02-07 12:16:10 -0700

    basic groundwork
    doesn't really have any functionality
    user interface completed

---
+++ source.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int welcomeScreen();	
+int encodeMessage();
+int decodeMessage();
+
+int testing;
+string fileChoice;
+
+int main() {
+	cout << "ImageLock Suite V1";
+	welcomeScreen();
+}
+
+int welcomeScreen() { //Allows the user to choose if they would like to encode or decode a message. Add nothing else here.
+	int EncodeOrDecode;
+	cout << "\nWould you like to ENCODE (1) or DECODE (2) text into an image file?\nInput: ";
+	cin >> EncodeOrDecode;
+	if (EncodeOrDecode == 1) {
+		encodeMessage();
+	}
+	if (EncodeOrDecode == 2) {
+		decodeMessage();
+	}
+	else
+	{
+		cout << "Invalid value. Please try again.";
+		welcomeScreen();
+	}
+	return EncodeOrDecode;
+}
+
+int encodeMessage() {
+	cout << "What (image) file would you like to encode a message into?\nInput: ";
+	cin >> fileChoice;
+	//return fileChoice;
+}
+
+int decodeMessage() {
+	cout << "What (image) file would you like to decode?\nInput: ";
+	cin >> fileChoice;
+	//return fileChoice;
+}
