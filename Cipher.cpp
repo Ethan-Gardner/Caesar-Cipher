ead0b1641c5f812be99427cced5558d0a18a1783
Ethan Gardner <gardnere@mdsd.org>
2024-03-04 12:57:01 -0700

    Update Cipher.cpp

--- Cipher.cpp
+++ Cipher.cpp
@@ -4,42 +4,51 @@
 
 using namespace std;
 
-string encrypt(const string & text, int s); // Use const reference for efficiency
+string encrypt(const string& text, int s);
 void encryptMenu();
 
 int main() {
-  cout << "Caesarean (Substitution) Cipher";
-  encryptMenu();
+    cout << "Caesarean (Substitution) Cipher";
+    encryptMenu();
 }
 
-void encryptMenu() { //Using void instead of string, because no "return" value is needed
-  string text;
-  int s;
+void encryptMenu() {
+    string text;
+    int s;
+    cout << "\nInput your message: ";
+    cin >> text;
 
-  cout << "\n[ENCODE] Input your message: ";
-  cin >> text;
-  cout << "[ENCODE] Shift your message: ";
-  cin >> s;
+    cout << "Shift your message: ";
+    cin >> s;
+ 
+    while (!std::cin) { // Error Checking
+        cin.clear();
+        cin.ignore(10000, '\n'); //10000 is a random big value
+        cout << "Invalid input. Enter an integer.\n Shift your message: ";
+        cin >> s;
 
-  cout << "Text : " << text <<
-    "\nShift: " << s <<
-    "\nCipher: " << encrypt(text, s) << endl; // Print newline for clarity
-}
-
-string encrypt(const string & text, int s) {
-  string result;
-  const int textSize = text.length(); // Cache the length to avoid recalculating
-
-  for (int i = 0; i < textSize; i++) {
-    char currentChar = text[i];
-
-    if (isalpha(currentChar)) {
-      char base = isupper(currentChar) ? 'A' : 'a';
-      result += static_cast < char > ((currentChar - base + s) % 26 + base);
-    } else {
-      result += currentChar; // Preserve non-alphabetic characters
     }
-  }
 
-  return result;
+    cout << "\nText : " << text <<
+        "\nShift: " << s <<
+        "\nCipher: " << encrypt(text, s) << endl;
+}
+
+string encrypt(const string& text, int s) {
+    string result;
+    const int textSize = text.length();
+
+    for (int i = 0; i < textSize; i++) {
+        char currentChar = text[i];
+
+        if (isalpha(currentChar)) {
+            char base = isupper(currentChar) ? 'A' : 'a';
+            result += static_cast <char> ((currentChar - base + s) % 26 + base);
+        }
+        else {
+            result += currentChar;
+        }
+    }
+
+    return result;
 }
